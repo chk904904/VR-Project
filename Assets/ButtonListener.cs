be24c200fa6a05a5887e743f816b7e7df70885ba
Kuan Chen <chenkuan904@gmail.com>
2021-03-11 11:09:24 -0500

    add score and combination

--- Assets/ButtonListener.cs
+++ Assets/ButtonListener.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using OculusSampleFramework;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using System;
 
 public class ButtonListener : MonoBehaviour
 {
@@ -13,140 +14,174 @@ public class ButtonListener : MonoBehaviour
     public UnityEvent contactEvent;
     public GameObject textBox; 
     public Text text;
+    public GameObject timer;
+    public Animator anim;
+
+
+
     private Renderer m_renderer;
     private List<int> patternIndexesLocal;
     private int curIndex;
+    private InteractableTool hand;
+    private int curPunching = 0;
+    private int numOfPunching = -1;
+    private int[] curCombo = new int[6];
+    private int score = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<ButtonController>().InteractableStateChanged.AddListener(InitiateEvent);
         m_renderer = GetComponent<Renderer>();
-        textBox = GameObject.Find("Text");
+        text.text = "Score: " + score.ToString();
         //initialize indexes
-        patternIndexesLocal = GameObject.Find("Text").GetComponent<PatternController>().patternIndexes;
-        curIndex = 0;
+        //patternIndexesLocal = GameObject.Find("Text").GetComponent<PatternController>().patternIndexes;
+        //curIndex = 0;
     }
 
+    public void endTraining()
+    {
+        GetComponent<ButtonController>().InteractableStateChanged.RemoveListener(InitiateEvent);
+        foreach(Transform child in textBox.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
     void InitiateEvent(InteractableStateArgs state)
     {
-        //if(state.NewInteractableState == InteractableState.ProximityState)
-        //{
-        //    proximityEvent.Invoke();
-        //} else if(state.NewInteractableState == InteractableState.ContactState)
-        //{
-        //    contactEvent.Invoke();
-        //} else 
+        
         if (state.NewInteractableState == InteractableState.ActionState)
         {
-            if (state.Tool.IsRightHandedTool)
+            if (timer.GetComponent<timer>().timeGetter() == 60)
             {
-                m_renderer.material.color = Color.green;
-            } else
-            {
-                m_renderer.material.color = Color.blue;
+                timer.GetComponent<timer>().StartTimer();
+                textBox.GetComponent<PatternController>().generatingCombo();
             }
-            text.text = "";
-            /*
-            greenIndex = 0; 
-            brownIndex = 1; 
-            redIndex = 2; 
-            pinkIndex = 3;
-            purpleIndex = 4; 
-            blueIndex = 5 
-            */
-            if (System.Math.Abs(state.Tool.Velocity.z) > System.Math.Abs(state.Tool.Velocity.x) && System.Math.Abs(state.Tool.Velocity.z) > System.Math.Abs(state.Tool.Velocity.y))
+            if (curCombo[curPunching] == punchingDetect(state))
             {
-                if (state.Tool.IsRightHandedTool)
-                {
-                    //TODO: check whether correct order
-                    GameObject green = textBox.transform.transform.GetChild(0).gameObject; 
-                    text.text = "Right Straight";
-                    if(curIndex == 0){
-                        green.SetActive(false);
-                        curIndex += 1;
-                    }
-                    else{
-                        curIndex = 0; 
-                    }
-                } else
-                {
-                    //TODO: check whether correct order
-                    GameObject brown = textBox.transform.transform.GetChild(1).gameObject;
-                    text.text = "Left Straight";
-                    if(curIndex == 1){
-                        brown.SetActive(false);
-                        curIndex += 1;
-                    }
-                    else{
-                        curIndex = 0; 
-                    }
-                }
-                
-            } else if (System.Math.Abs(state.Tool.Velocity.x) > System.Math.Abs(state.Tool.Velocity.y) && System.Math.Abs(state.Tool.Velocity.x) > System.Math.Abs(state.Tool.Velocity.z))
-            {
-                if (state.Tool.IsRightHandedTool)
-                {
-                    GameObject red = textBox.transform.GetChild(2).gameObject;
-                    text.text = "Right Hook";
-                    if(curIndex == 2){
-                        red.SetActive(false);
-                        curIndex += 1;
-                    }
-                    else{
-                        curIndex = 0; 
-                    }
-                }
-                else
-                {
-                    GameObject pink = textBox.transform.GetChild(3).gameObject;
-                    text.text = "Left Hook";
-                    if(curIndex == 3){
-                        pink.SetActive(false);
-                        curIndex += 1;
-                    }
-                    else{
-                        curIndex = 0; 
-                    }
-                }
-            } else
-            {
-                if (state.Tool.IsRightHandedTool)
-                {
-                    GameObject purple = textBox.transform.GetChild(4).gameObject;
-                    text.text = "Right Uppercut";
-                    if(curIndex == 4){
-                        purple.SetActive(false);
-                        curIndex += 1;
-                    }
-                    else{
-                        curIndex = 0; 
-                    }
-                }
-                else
-                {
-                    GameObject blue = textBox.transform.GetChild(5).gameObject;
-                    text.text = "Left Uppercut";
-                    if(curIndex == 5){
-                        blue.SetActive(false);
-                        curIndex += 1;
-                    }
-                    else{
-                        curIndex = 0; 
-                    }
-                }
+                textBox.transform.GetChild(curPunching).gameObject.SetActive(false);
+                curPunching++;
             }
-            //text.text = state.Tool.Velocity.x + " " + state.Tool.Velocity.y + " " + state.Tool.Velocity.z;
-            //actionEvent.Invoke();
+
+            if(curPunching == numOfPunching)
+            {
+                numOfPunching = -1;
+                curPunching = 0;
+                Array.Clear(curCombo, 0, curCombo.Length);
+                score++;
+                text.text = "Score: " + score.ToString();
+                textBox.GetComponent<PatternController>().generatingCombo();
+            }
         }
         else
         {
             defaultEvent.Invoke();
         }
+        //anim.SetBool("isPunched", false);
+    }
+    public void comboSetter(int[] temp)
+    {
+        Array.Clear(curCombo, 0, curCombo.Length);
+        temp.CopyTo(curCombo, 0);
+        numOfPunching = temp.Length;
+        curPunching = 0;
+
+    }
+    private int punchingDetect(InteractableStateArgs state)
+    {
+        float x = System.Math.Abs(state.Tool.Velocity.x);
+        float y = System.Math.Abs(state.Tool.Velocity.y);
+        float z = System.Math.Abs(state.Tool.Velocity.z);
+
+        if (z > x && z > y)
+        {
+            if (state.Tool.IsRightHandedTool)
+            {
+                StartCoroutine(BagBouncing(2));
+                return 2;
+            }
+            else
+            {
+                StartCoroutine(BagBouncing(1));
+                return 1;
+            }
+
+        }
+        else if (x > y && x > z)
+        {
+            if (state.Tool.IsRightHandedTool)
+            {
+                StartCoroutine(BagBouncing(4));
+                return 4;
+            }
+            else
+            {
+                StartCoroutine(BagBouncing(3));
+                return 3;
+            }
+        }
+        else
+        {
+            if (state.Tool.IsRightHandedTool)
+            {
+                StartCoroutine(BagBouncing(6));
+                return 6;
+            }
+            else
+            {
+                StartCoroutine(BagBouncing(5));
+                return 5;
+            }
+        }
     }
     // Update is called once per frame
+
+    IEnumerator BagBouncing(int temp)
+    {
+        switch(temp)
+        {
+            case 1:
+                anim.SetBool("jab", true);
+                yield return new WaitForSeconds(0.5f);
+                anim.SetBool("jab", false);
+                break;
+            case 2:
+                anim.SetBool("cross", true);
+                yield return new WaitForSeconds(0.5f);
+                anim.SetBool("cross", false);
+                break;
+            case 3:
+                anim.SetBool("hook", true);
+                yield return new WaitForSeconds(0.5f);
+                anim.SetBool("hook", false);
+                break;
+            case 4:
+                anim.SetBool("hook", true);
+                yield return new WaitForSeconds(0.5f);
+                anim.SetBool("hook", false);
+                break;
+            case 5:
+                anim.SetBool("uppercut", true);
+                yield return new WaitForSeconds(0.5f);
+                anim.SetBool("uppercut", false);
+                break;
+            case 6:
+                anim.SetBool("uppercut", true);
+                yield return new WaitForSeconds(0.5f);
+                anim.SetBool("uppercut", false);
+                break;
+            default:
+                break;
+
+        }
+        
+    }
     void Update()
     {
-        
+        //if(hand != null)
+        //{
+        //    valueDisplay.text = (int)(100*hand.ToolTransform.position.x) + " " + (int)(100*hand.ToolTransform.position.y) + " " + (int)(100*hand.ToolTransform.position.z);
+        //}
     }
 }
--- Assets/PatternController.cs
+++ Assets/PatternController.cs
@@ -8,30 +8,60 @@ using UnityEngine.UI;
 public class PatternController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public List<int> patternIndexes = new List<int>();
+    //public List<int> patternIndexes = new List<int>();
+    public ButtonListener listener;
+    [SerializeField]
+    private int[][] combos = { 
+        new int[]{1,1}, 
+        new int[]{1,1,2},
+        new int[]{1,2},
+        new int[]{1,2,1},
+        new int[]{1,2,1,2},
+        new int[]{1,2,3,2},
+        new int[]{1,6,3,2}
+    };
 
-
+    private string[] punchings = { "Jab", "Right Cross", "Left Hook", "Right Hook", "Left Uppercut", "Right Uppercut" };
+    private Color[] punchingColors = { Color.red, Color.green, Color.blue, Color.yellow, Color.white, Color.grey};
+    private GameObject[] squares = new GameObject[6];
     void Start()
     {
-    	for(int i = 0; i < patternIndexes.Count; i++){
-    		int curPatternIndex = patternIndexes[i];
-    		GameObject curObject = this.gameObject.transform.GetChild(curPatternIndex).gameObject;
-    		curObject.SetActive(false);
-    	}
+    	//for(int i = 0; i < patternIndexes.Count; i++){
+    	//	int curPatternIndex = patternIndexes[i];
+    	//	GameObject curObject = this.gameObject.transform.GetChild(curPatternIndex).gameObject;
+    	//	curObject.SetActive(false);
+    	//}
+        for (int i = 0; i < 6; i++)
+        {
+            squares[i] = this.gameObject.transform.GetChild(i).gameObject;
+            squares[i].SetActive(false);
+        }
     }
-
+    public void generatingCombo()
+    {
+        System.Random rd = new System.Random();
+        int comboIndex = rd.Next(0, combos.Length);
+        for (int i = 0; i < combos[comboIndex].Length; i++)
+        {
+            squares[i].GetComponent<Image>().color = punchingColors[combos[comboIndex][i] - 1];
+            squares[i].GetComponentInChildren<Text>().text = punchings[combos[comboIndex][i] - 1];
+            squares[i].SetActive(true);
+        }
+        listener.comboSetter(combos[comboIndex]);
+    }
     // Update is called once per frame
     void Update()
     {
-		StartCoroutine(playPattern());
+		//StartCoroutine(playPattern());
     }
 
-    IEnumerator playPattern(){
-    	for(int i = 0; i < patternIndexes.Count; i++){
-    		int curPatternIndex = patternIndexes[i];
-    		GameObject curObject = this.gameObject.transform.GetChild(curPatternIndex).gameObject;
-    		curObject.SetActive(true);
-    		yield return new WaitForSeconds(0.5f);
-    	}
-    }
+    //IEnumerator playPattern(){
+    //	for(int i = 0; i < patternIndexes.Count; i++){
+    //		int curPatternIndex = patternIndexes[i];
+    //		GameObject curObject = this.gameObject.transform.GetChild(curPatternIndex).gameObject;
+    //		curObject.SetActive(true);
+    //		yield return new WaitForSeconds(0.5f);
+    //	}
+    //}
+    
 }
