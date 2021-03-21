b7e58814ef2477b0c0f84fa9f135feded6c72aa3
Kuan Chen <chenkuan904@gmail.com>
2021-03-21 09:50:44 -0400

    Switch to controller

---
+++ Assets/ControllerDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text text;
+    private int counter = 0;
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        GloveFollowing GF = other.gameObject.GetComponent<GloveFollowing>();
+        text.text = other.gameObject.name+GF.velocity;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        text.text = collision.gameObject.name + "collision";
+        //text.text = collision.relativeVelocity.x + ", " + collision.relativeVelocity.y + ", " + collision.relativeVelocity.z;
+        //text.text = collision.gameObject.name;
+        //text.text = collision.gameObject.name + ": " + collision.relativeVelocity.x.ToString() + " " + collision.relativeVelocity.y.ToString() + " " + collision.relativeVelocity.z.ToString();
+    }
+}
---
+++ Assets/GloveFollowing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GloveFollowing : MonoBehaviour
+{
+    bool isLeft = true;
+    public Text text;
+    private Vector3 m_lastPos;
+    private Vector3 m_lastVelocity;
+    public float speed = 0f;
+    public Vector3 velocity = new Vector3(0, 0, 0);
+    public Transform root;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (this.gameObject.name.Equals("RightGlove"))
+        {
+            isLeft = false;
+        }
+        m_lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localPosition.magnitude != 0)
+        {
+            transform.localPosition = new Vector3(0, 0, 0);
+            if(isLeft)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 90);
+            } else
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, -90);
+            }
+            
+        }
+        //Vector3 distance = transform.position - origin.position;
+        //Vector3 relativePosition = Vector3.zero;
+        //relativePosition.x = Vector3.Dot(distance, origin.right.normalized);
+        //relativePosition.y = Vector3.Dot(distance, origin.up.normalized);
+        //relativePosition.z = Vector3.Dot(distance, origin.forward.normalized);
+        velocity = (transform.position - m_lastPos) / Time.deltaTime;
+        m_lastPos = transform.position;
+
+    }
+}
