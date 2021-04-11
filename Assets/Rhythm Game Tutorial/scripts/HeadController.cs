2591c643e017fd254eb6761f070efffafa1fb4cf
Yilin Wang <lwang@smith.edu>
2021-04-11 09:35:12 -0400

    Head and rod controllers

---
+++ Assets/Rhythm Game Tutorial/scripts/HeadController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OculusSampleFramework;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System;
+using System.Text;
+
+public class HeadController : MonoBehaviour
+{
+    public GameObject headAnchor;
+    public GameObject txt; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = headAnchor.transform.position;
+    }
+
+	void OnCollisionEnter(Collision collision)
+    {
+        txt.GetComponent<Text>().text = "Collision!";
+    }
+
+}
---
+++ Assets/Rhythm Game Tutorial/scripts/RodController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RodController : MonoBehaviour
+{	
+	public GameObject leftRod; 
+	public GameObject rightRod;
+	public int xLeftAngle; 
+	public int yLeftAngle; 
+	public int xRightAngle; 
+	public int yRightAngle;
+	private int ctr; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        xLeftAngle = 75; 
+        yLeftAngle = -30;
+        xRightAngle = 75;
+        yRightAngle = 45; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	ctr = ctr + 1; 
+        leftRod.transform.Rotate(ctr%xLeftAngle, ctr%yLeftAngle,0);
+        rightRod.transform.Rotate(ctr%xRightAngle, ctr%yRightAngle,0);
+    }
+}
