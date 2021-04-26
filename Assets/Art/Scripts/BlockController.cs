0831cc8513f236125b1fc9f295b1f6dc8fbbfa9f
yilinLucywang <72194580+yilinLucywang@users.noreply.github.com>
2021-04-25 21:09:28 -0400

    Scripts for prototype 2

---
+++ Assets/Art/Scripts/BlockController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+	public float block_speed; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	block_speed = -0.1f;
+    	Debug.Log(block_speed);
+        transform.position += new Vector3(0f, 0f, block_speed);
+    }
+}
